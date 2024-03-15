c8d82c9b751917c8276f4cc26f7be9e6fba6dfa9
Asadullokh27 <asadullohtojiyev696@gmail.com>
2024-03-15 10:01:12 +0500

    Add project files.

---
+++ src/BookStore.Presentation/MVC/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+using BookStore.Application.useCases.Books.Commands;
+using BookStore.Application.useCases.Books.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+using System.Diagnostics;
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public HomeController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var getAllBooks = new GetAllBooksQuery();
+            var books = await _mediator.Send(getAllBooks);
+            return View(books);
+        }
+
+        public async Task<IActionResult> Delete(long id)
+        {
+            var deleteBook = new DeleteBookCommand()
+            {
+                Id = id
+            };
+            await _mediator.Send(deleteBook);
+            return RedirectToAction(actionName: nameof(Index));
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
